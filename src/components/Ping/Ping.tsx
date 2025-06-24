5b9275ef937e2017defdbd45518a6cd964d29a40
levlong <hi.newagenewera@gmail.com>
2025-06-24 21:42:34 +0700

    [ADD] get from DB

--- src/components/Ping/Ping.tsx
+++ src/components/Ping/Ping.tsx
@@ -8,7 +8,7 @@ export default function Ping() {
   useEffect(() => {
     const fetchLog = async () => {
       try {
-        const res = await axios.get("http://localhost:8080/api/monitor");
+        const res = await axios.get("/api/monitor");
         setLog(res.data);
       } catch (error) {
         console.log(error);
